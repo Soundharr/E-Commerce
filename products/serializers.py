c6b3c8ee111e784a618891b0cb018079bbf14548
Soundharr <116069963+Soundharr@users.noreply.github.com>
2025-07-09 21:15:32 +0530

    Image Url

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Category
 
-
 # Category Serializer
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,11 +14,18 @@ class ProductSerializer(serializers.ModelSerializer):
     category_id = serializers.PrimaryKeyRelatedField(
         queryset=Category.objects.all(), source='category', write_only=True
     )
+    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = [
             'id', 'title', 'slug', 'price', 'discount_price', 'description',
-            'stock', 'is_active', 'image', 'created_at', 'updated_at',
+            'stock', 'is_active', 'image', 'image_url', 'created_at', 'updated_at',
             'category', 'category_id'
         ]
+
+    def get_image_url(self, obj):
+        # Ensure we return the full URL for the image (MEDIA_URL + image path)
+        if obj.image:
+            return obj.image.url
+        return None
