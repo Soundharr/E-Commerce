2bfbcc90ce9c35ea16b4299274836ecd1529f350
Soundharr <116069963+Soundharr@users.noreply.github.com>
2025-07-15 15:20:36 +0530

    Update settings cloudinary

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Category, Product
+import cloudinary.uploader
 
 class CategorySerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()  # Added to return Cloudinary URL
@@ -7,12 +8,20 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'image', 'image_url']
+        read_only_fields = ['image_url']
 
-    def get_image_url(self, obj):
-        # This returns the Cloudinary URL for the image
-        if obj.image:
-            return obj.image.url  # Cloudinary URL will automatically be generated
-        return None
+    # def get_image_url(self, obj):
+    #     # This returns the Cloudinary URL for the image
+    #     if obj.image:
+    #         return obj.image.url  # Cloudinary URL will automatically be generated
+    #     return None
+    def create(self, validated_data):
+        image = validated_data.pop('image')
+        # Upload image file to Cloudinary
+        upload_result = cloudinary.uploader.upload(image)
+        image_url = upload_result.get('secure_url')
+        category = Category.objects.create(image_url=image_url, **validated_data)
+        return category
 
 
 class ProductSerializer(serializers.ModelSerializer):
